647c19be6c937b48e334e7fbe3d06c34a950d09c
Shivansh Vyas <shivanshvyas2207@gmail.com>
2026-07-24 16:22:56 +0530

    Add supplied hero background artwork

--- components/home/HeroBackground.tsx
+++ components/home/HeroBackground.tsx
@@ -1,18 +1,8 @@
 export function HeroBackground() {
   return (
     <div className="hero-background" aria-hidden="true">
-      <div className="hero-background__glow hero-background__glow--blue" />
-      <div className="hero-background__glow hero-background__glow--red" />
-      <div className="hero-background__web" />
-      <div className="hero-background__dots hero-background__dots--blue" />
-      <div className="hero-background__dots hero-background__dots--red" />
-      <div className="hero-background__shard hero-background__shard--one" />
-      <div className="hero-background__shard hero-background__shard--two" />
-      <div className="hero-background__shard hero-background__shard--three" />
-      <div className="hero-background__streak hero-background__streak--one" />
-      <div className="hero-background__streak hero-background__streak--two" />
-      <div className="hero-background__streak hero-background__streak--three" />
-      <div className="hero-background__city" />
+      <div className="hero-background__image" />
+      <div className="hero-background__wash" />
     </div>
   );
 }
